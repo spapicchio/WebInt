3336f0efc6ae8f4c0f369312fb159c52bbdbdcb1
Simone Papicchio <56116742+spapicchio@users.noreply.github.com>
2022-01-22 09:29:52 +0100

    Update BdeSide.vue

--- Assignment-2/eurecom-one/src/components/BdeSide.vue
+++ Assignment-2/eurecom-one/src/components/BdeSide.vue
@@ -2,7 +2,7 @@
 <div class=container>
         <img
         src="../assets/bde.png"
-        :alt="BDE"
+        alt="BDE"
         />
 
         <h1><b>BDE</b> is the best Student Association!</h1>
@@ -17,11 +17,78 @@
             <p> 
                 Conctact us to have a clearer idea on what you can do with us!
                 <br>
-                    <button class="button is-dark is-medium"  @click="onClick">
+                    <button class="button is-dark is-medium"  @click=onClick>
                         Contact us
                     </button>
             </p>
-        </div>
+
+            <div class=contact-form v-if=showForm>
+
+                <div class="field">
+                    <label class="label">Email</label>
+                    <div class="control has-icons-left has-icons-right">
+                        <input class="input is-danger" type="email" placeholder="Email input" value="hello@">
+                        <span class="icon is-small is-left">
+                        <i class="fas fa-envelope"></i>
+                        </span>
+                        <span class="icon is-small is-right">
+                        <i class="fas fa-exclamation-triangle"></i>
+                        </span>
+                    </div>
+                    <p class="help is-danger">This email is invalid</p>
+                    </div>
+
+                    <div class="field">
+                    <label class="label">Subject</label>
+                    <div class="control">
+                        <div class="select">
+                        <select>
+                            <option>Select dropdown</option>
+                            <option>With options</option>
+                        </select>
+                        </div>
+                    </div>
+                    </div>
+
+                    <div class="field">
+                    <label class="label">Message</label>
+                    <div class="control">
+                        <textarea class="textarea" placeholder="Textarea"></textarea>
+                    </div>
+                    </div>
+
+                    <div class="field">
+                    <div class="control">
+                        <label class="checkbox">
+                        <input type="checkbox">
+                        I agree to the <a href="#">terms and conditions</a>
+                        </label>
+                    </div>
+                    </div>
+
+                    <div class="field">
+                    <div class="control">
+                        <label class="radio">
+                        <input type="radio" name="question">
+                        Yes
+                        </label>
+                        <label class="radio">
+                        <input type="radio" name="question">
+                        No
+                        </label>
+                    </div>
+                    </div>
+
+                    <div class="field is-grouped">
+                    <div class="control">
+                        <button class="button is-link">Submit</button>
+                    </div>
+                    <div class="control">
+                        <button class="button is-link is-light">Cancel</button>
+                    </div>
+                    </div>
+                                </div>
+                            </div>
 
 </div>
 
@@ -31,10 +98,17 @@
 export default {
 
 data(){
+        var obj = JSON.parse(sessionStorage.getItem("account"));
+        console.log(obj)
         return {
-          
+           showForm: true 
         }
     },
+methods:{
+    onClick(){
+        console.log("clicked")
+    }
+}
 
 };
 </script>
@@ -67,6 +141,7 @@ data(){
 }
 
 button{
+    margin: 30px auto 5px 25%;
     max-width: 300px;
 }
 h1{
