67e8ed109ffd5807104e8f03bc28eb0c080e2b6e
Simone Papicchio <56116742+spapicchio@users.noreply.github.com>
2022-01-23 20:43:17 +0100

    update email

--- Assignment-2/eurecom-one/src/main.js
+++ Assignment-2/eurecom-one/src/main.js
@@ -2,4 +2,20 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBtjgUoTgCsFkaXv75hl5_Cm_VirdsI1rM",
+    authDomain: "assignment-2-webint.firebaseapp.com",
+    projectId: "assignment-2-webint",
+    storageBucket: "assignment-2-webint.appspot.com",
+    messagingSenderId: "142166599801",
+    appId: "1:142166599801:web:6e8ec1a109edff4e588ca8",
+    measurementId: "G-N8H2MC7LVK"
+  };
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+console.log(firebase);
+
 createApp(App).use(router).mount("#app");
--- Assignment-2/eurecom-one/src/views/Mail.vue
+++ Assignment-2/eurecom-one/src/views/Mail.vue
@@ -1,9 +1,36 @@
 <template>
    <div class="wrapper">
     <Header /> 
-    
     <Nav />
+    <!-- inside there are the otpions for the email -->
+    <div class="aside-1 container">
+        <div class="email-option"><button class="button" @click="emailOption('Inbox')"> Inbox</button></div>
+        <div class="email-option"><button class="button" @click="emailOption('Sent')"> Sent</button></div>
+        <div class="email-option"><button class="button" @click="emailOption('Inbox')"> Spam</button></div>
+    </div>
+
+    <!-- inside there are the emails -->
+    <div class="card-email">
+        <h1 class="title is-3">{{nameCard}}</h1>
+        <table v-if="!loading">
+        <tr>
+            <th>Email</th>
+            <th>Name</th>
+            <th>Object</th>
+            <th>See Email</th>
+        </tr>
+        <tr v-for="email in emails" :key="email.id">
+            <td>{{ email.email }}</td>
+            <td>{{ email.name }}</td>
+            <td>{{ email.object }}</td>
+            <td><button class="button"> Open </button></td>
+        </tr>
+        </table>
     
+    </div>
+
+    <!-- this is used to send a new email -->
+    <div class="card-new-email"></div>
 
     <Footer />
 </div>
@@ -14,37 +41,105 @@
 import Header from '../components/Header.vue';
 import Footer from '../components/Footer.vue';
 import Nav from '../components/Nav.vue';
+import { getFirestore, doc, getDoc } from "firebase/firestore"
+
+
 
 
 export default {
-  name: "Mail",
-  data(){
+    name: "Mail",
+    data(){
+        this.updateData();
+        var obj = JSON.parse(sessionStorage.getItem("account"));
+        console.log(obj)
+
+        return {
+            nameCard: "Inbox",
+            loading: false,
+            emails: [],
+        }
+    },
+    methods:{
+        emailOption(name){
+            this.nameCard = name;
+        },
+        async updateData(){
+            // firestore instance
+ 
+            // firestore instance
+            const db = getFirestore();
+            // reference inside the firestore db
+            const ref = doc(db, "users", "simone");
+
+            const docSnap = await getDoc(ref);
+
+            if (docSnap.exists()) {
+            this.emails = docSnap.data().inbox;
+            } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+            }
 
 
-    var obj = JSON.parse(sessionStorage.getItem("account"));
-    console.log(obj)
-
-    return {
-      
-    }
-
-  },
-
-  components: {
-    Footer,
-    Nav,
-    Header,
-  
-  },
-};
+            
+        },
+    },
+    components: {
+        Footer,
+        Nav,
+        Header,
+    
+    },
+    };
 </script>
 
 <style scoped>
+
+.container{
+    display: inline-block;
+    background-color: white;
+    border: 1px solid;
+    box-shadow: 0px 5px 10px 0px #888888;
+    margin: 5px 5px 10px 10px;
+}
+
+.email-option{
+    border-bottom: 1px solid;
+
+}
+.email-option button{
+    width: 100%;
+    height: 100%;
+    
+}
+
+table{
+    table-layout: fixed;
+    max-width: 100%;
+    margin: 0% auto 0% auto;
+    text-align: left;
+    border-collapse: separate;
+    border-spacing: 0px 7px;
+}
+
+table td,
+table th{
+    padding:0 15px 0 15px;
+    word-wrap: break-word;
+}
+
+table th{
+    border-bottom: 2pt solid black;
+}
+
+table td{
+    border-bottom: 0.1pt solid gray;
+}
+
 .wrapper {
     display: grid;
-    grid-template-rows: 150px 50px 50% 50% 20%;
-    grid-template-columns: 20% 80% 20%;
-    
+    grid-template-columns: 20% 20% 20% 20% 20%;
+    grid-template-rows: 150px 50px 20% 20% 20% 20%;
 }
 .header{
     grid-column: 1/6;
@@ -52,14 +147,35 @@ export default {
 }
 .nav{
     grid-row-start: 2;
-    grid-column: 1/4;
+    grid-column: 1/6;
 }
+.main {
+    text-align: center;
+    background:  rgba(218, 218, 218, 0.151);
+    grid-column: 2/5;
+    grid-row: 3/7;
+}
+.card-email,
+.card-new-email{
+    text-align: center;
+    grid-column: 2/6;
+    grid-row: 3/7;
+    box-shadow: 0px 5px 10px 0px #888888;
+    margin: 5px 5px 10px 10px;
+}
+
+
+
+.aside-1 {
+    background: rgba(134, 134, 134, 0.151);
+    grid-column: 1;
+    grid-row: 3/7;
+}
+
 .footer{
-    grid-row-start: 4;
-    grid-column: 1/4;
+    grid-row: 7;
+    grid-column: 1/6;
 }
-
-
 @media all and (min-width: 600px) {
   .aside { flex: 1 0 0; }
 }
